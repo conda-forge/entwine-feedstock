5465c0e67a3f0bac552957d6cfdc33dc0750748f
Howard Butler <hobu.inc@gmail.com>
2025-09-05 11:12:05 -0500

    set _LIBCPP_DISABLE_AVAILABILITY

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,11 @@ then
    export LDFLAGS="$LDFLAGS -Wl,-rpath-link,${PREFIX}/lib"
 fi
 
+if [[ "${target_platform}" == "osx-64" ]]; then
+  # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+  export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 
 mkdir build
 cd build
